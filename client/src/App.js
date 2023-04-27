2ca3ee3015e58d903429d8c2d866bcb59f3d97a6
dwiputrapurnawa <putrapurnawa@gmail.com>
2023-04-28 00:58:45 +0800

    jpg and png to pdf

---
+++ client/src/App.js
@@ -0,0 +1,98 @@
+import React, { useState } from "react";
+import { Upload, Button, message, Space, Divider, Select, Image } from "antd";
+import { InboxOutlined, DownloadOutlined, UploadOutlined } from "@ant-design/icons"
+import logo from "../src/assets/images/konverta.png";
+
+function App() {
+
+  const [fileList, setFileList] = useState([]);
+  const [uploading, setUploading] = useState(false);
+  const [pdf, setPDF] = useState("");
+
+  const sizeOptions = [
+    {
+      label: "A4",
+      value: "A4"
+    },
+    {
+      label: "A5",
+      value: "A5"
+    }
+  ]
+
+  const handleUpload = async () => {
+
+    const formData = new FormData();
+
+    fileList.forEach((file) => {
+      formData.append("files[]", file);
+    });
+
+    setUploading(true);
+
+    const url = "http://localhost:35033/api/imgtopdf";
+    const options = {
+      method: "POST",
+      body: formData,
+    }
+
+    const response = await fetch(url, options);
+
+    const data = await response.json();
+
+    if(data.success) {
+      setFileList([]);
+      setPDF(data.pdf)
+      message.success("Upload successfully.");
+    } else {
+      message.error("Upload failed.")
+    }
+
+    setUploading(false);
+
+  }
+
+  const props = {
+    name: "file",
+    beforeUpload: (file) => {
+      setFileList([...fileList, file]);
+      return false;
+    },
+    onRemove: (file) => {
+      const index = fileList.indexOf(file);
+      const newFileList = fileList.slice();
+      newFileList.splice(index, 1);
+      setFileList(newFileList);
+    },
+    fileList,
+  }
+
+  return (
+    <div style={{ display: 'block', margin: "auto", width: "50%" }}>
+      <Space direction="vertical" style={{ width: "100%" }}>
+        <Image width={200} src={logo} preview={false} />
+        <Divider />
+        <Upload.Dragger {...props}>
+        <p className="ant-upload-drag-icon">
+        <InboxOutlined />
+        </p>
+        <p className="ant-upload-text">Click or drag file to this area to upload</p>
+        <p className="ant-upload-hint">
+         Convert JPG and PNG to PDF
+        </p>
+        </Upload.Dragger>
+
+        <Divider />
+        <Space>
+          <Button icon={<UploadOutlined />} type="primary" size="large" disabled={fileList.length === 0 ? true : false} onClick={handleUpload} loading={uploading}>{uploading ? "Converting" : "Convert"}</Button>
+
+          {/* <Select type="primary" defaultValue="Select Size" options={sizeOptions} /> */}
+
+          <Button disabled={pdf === "" ? true : false} icon={<DownloadOutlined />} type="primary" shape="round" size="large" href={"http://localhost:35033/pdf/" + pdf} target="__blank">Download PDF</Button>
+        </Space>
+      </Space>
+    </div>
+  );
+}
+
+export default App;
---
+++ server/server.js
@@ -0,0 +1,61 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const multer = require("multer");
+const imageToPDF = require("image-to-pdf");
+const fs = require("fs");
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, __dirname + "/uploads/images");
+    },
+    filename: (req, file, cb) => {
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+        cb(null, file.originalname);
+    }
+});
+
+const uploadImage = multer({ storage: storage }).array("files[]");
+
+const app = express();
+
+app.use(express.json());
+app.use(express.static(__dirname + "/uploads"));
+
+app.use(cors({
+    origin: "http://localhost:3000",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true
+}));
+
+app.post("/api/imgtopdf", (req, res) => {
+    uploadImage(req, res, (err) => {
+        if(err) {
+            console.log(err);
+            res.send({success: false, message: "Failed to upload image"});
+        }
+
+        const pages = req.files.map((value) => value.path);
+
+        let random = (Math.random() + 1).toString(36).substring(2);
+
+        const pdfTitle = random + ".pdf"
+
+        imageToPDF(pages, imageToPDF.sizes.A4).pipe(fs.createWriteStream(__dirname + "/uploads/pdf/" + pdfTitle));
+
+        res.send({success: true, pdf: pdfTitle})
+    })
+});
+
+
+
+
+app.listen(process.env.SERVER_PORT, () => {
+    console.log("Server running at port " + process.env.SERVER_PORT);
+})
+
+
+
+
+
+
